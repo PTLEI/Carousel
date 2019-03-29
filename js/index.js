c57c046568813ec1c6b1a8e14174284edd1da9ed
PTLEI <541811902@qq.com>
2019-03-29 11:21:00 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,141 @@
+// 判断某个元素是否为指定元素的子元素或本身
+jQuery.fn.isChildAndSelfOf = function (b) {
+  return (this.closest(b).length > 0);
+};
+
+$(document).ready(function () {
+  // 初始化轮播定位器宽度
+  var carousel = $('.carousel').eq(0);
+  carousel.height(carousel.width() - 510);
+
+  // 初始化轮播适应
+  adaptCarou();
+
+  // 初始化轮播图
+  var options = {
+    el: document.getElementsByClassName('carou-list')[0],
+    imgs: [
+      './imgs/index1.jpg',
+      './imgs/index2.jpg',
+      './imgs/index3.jpg',
+      './imgs/index4.jpg',
+    ],
+    timeSpace: 3
+  };
+  var carou = new Carousel(options);
+
+  // 开始轮播
+  carou.start();
+
+  // 绑定轮播的hover暂停事件
+  carousel.mouseenter(function (e) {
+    var claName = e.target.className;
+    var reg = /(search)|(^s-)/;
+    if (!reg.test(claName)) {
+      carou.stop();
+    }
+  })
+
+  // 判断是否是有效的mouseout触发
+  function checkUsefulAction(e) {
+    var target = e.target;
+    var relatedTarget = e.relatedTarget;
+    var claName = target ? target.className : null;
+    var relaClaName = relatedTarget ? relatedTarget.className : null;
+    var reg = /(search)|(^s-)/;
+    if (!reg.test(claName)) {
+      if (relatedTarget === null || !$(relatedTarget).isChildAndSelfOf('.carousel') || reg.test(relaClaName)) {
+        return true;
+      }
+    } else {
+      if (!reg.test(relaClaName) && $(relatedTarget).isChildAndSelfOf('.carousel')) {
+        return false;
+      }
+      return 'not useful';
+    }
+  }
+
+  carousel.mouseout(function (e) {
+    var res = checkUsefulAction(e);
+
+    if (res === true) {
+      carou.restart();
+      return;
+    }
+    if (res === false) {
+      carou.stop();
+      return;
+    }
+    return;
+  })
+
+  // 绑定箭头点击切换轮播
+  var leftIcon = $('.left-icon').eq(0);
+  var rightIcon = $('.right-icon').eq(0);
+  leftIcon.click(function () {
+    carou.leftAni();
+  });
+  rightIcon.click(function () {
+    carou.rightAni();
+  });
+
+  // 绑定箭头显示
+  var leftBody = $('.leftBody').eq(0);
+  var rightBody = $('.rightBody').eq(0);
+  leftBody.mouseenter(function () {
+    leftIcon.animate({
+      opacity: 1
+    }, 'fast');
+  })
+  leftBody.mouseleave(function () {
+    leftIcon.animate({
+      opacity: 0
+    }, 'fast');
+  })
+  rightBody.mouseenter(function () {
+    rightIcon.animate({
+      opacity: 1
+    }, 'fast');
+  })
+  rightBody.mouseleave(function () {
+    rightIcon.animate({
+      opacity: 0
+    }, 'fast');
+  })
+})
+
+// 轮播调整
+function adaptCarou() {
+
+  var headWidth = $('.headBox').eq(0).width();
+  var carouWidth = $('.carousel').eq(0).width();
+  var carouHeight = $('.carousel').eq(0).height() - 300 - $('.search').eq(0).height();
+  var leftBody = $('.leftBody').eq(0);
+  var rightBody = $('.rightBody').eq(0);
+  // 设置左右两边箭头区的宽高
+  leftBody.width(headWidth / 3);
+  leftBody.height(carouHeight);
+  rightBody.width(headWidth / 3);
+  rightBody.height(carouHeight);
+  // 左右两边箭头区到窗口边缘的距离
+  var l_r_space = (carouWidth - headWidth) / 2;
+  // 设置两边箭头区位置
+  leftBody.css('left', l_r_space + 'px');
+  rightBody.css('right', l_r_space + 'px');
+  // 设置箭头位置
+  var leftIcon = $('.left-icon').eq(0);
+  var rightIcon = $('.right-icon').eq(0);
+  var top = (carouHeight - leftIcon.height()) / 2;
+  leftIcon.css({
+    top: top + 'px',
+    opacity: 0
+  });
+  rightIcon.css({
+    top: top + 'px',
+    opacity: 0
+  });
+  // 设置轮播文字位置
+  var carouTag = $('.carou-tag');
+  var tagTop = (carouHeight - carouTag.height()) / 2 ;
+  carouTag.css('top', tagTop + 'px');
+}
